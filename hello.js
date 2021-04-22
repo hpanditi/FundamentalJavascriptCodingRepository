6cd7d538a8ff82c824d9d201dccc99a6deddb6a4
Hemanth Panditi <panditih@mail.gvsu.edu>
2021-04-22 19:52:43 -0400

    Made changes

--- hello.js
+++ hello.js
@@ -1,16 +1,8 @@
 console.log("Hello World. This is Hemanth Panditi");
 
-console.log("Hey I am coding in Javascript");
+console.log("Welcome to a Javascript website");
 
-console.log("This is super cool language");
 
-console.log("This is pretty neat. This is very interesting!!");
-
-console.log("Howdy Partner. How ya'll doin?");
-
-console.log("What up people");
-
-console.log("How ya'll doing?");
 
 
 
