87140a1a0ddf8515d9f37c6421d7ca4b98787790
hpanditi <59629153+hpanditi@users.noreply.github.com>
2021-04-22 17:53:59 -0400

    Added new file

---
+++ hello.js
@@ -0,0 +1,11 @@
+console.log("Hello World. This is Hemanth");
+
+console.log("Hey I am coding in Javascript");
+
+console.log("This is super cool language");
+
+console.log("This is pretty neat. This is very interesting!!");
+
+console.log("Howdy Partner. How ya'll doin?");
+
+
