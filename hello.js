5fbba642e70f6bf07e6626481c49af13b51afbe1
Hemanth Panditi <panditih@mail.gvsu.edu>
2021-04-22 20:19:57 -0400

    Update file

--- hello.js
+++ hello.js
@@ -4,25 +4,60 @@ console.log("Welcome to a Javascript website");
 
 // var, let, const
 
-let age = 30;
-
-console.log(age);
-
-let score;
-
-score = 10;
-
-console.log(score);
-
-// String, Numbers, Boolean, null, undefined, Symbol
-
 const name = 'John';
 const age = 30;
-const isCool = true;
-const rating = 4.5;
-const x = null;
-const y = undefined;
-let z;
+
+// Concatenation
+
+console.log("My name is " + name + " and I am " + age + " years old.");
+
+
+// string
+
+const s = 'Hello World!';
+
+console.log(s);
+console.log(s.length);
+console.log(s.substring(0, 5));
+
+console.log(s.split(''));
+
+const t = 'technology, computers, IT, code';
+
+console.log(t);
+
+// Arrays - variables that hold multiple values
+
+/*
+Multi
+Line
+comment
+*/
+
+const numbers = new Array(1,2,3,4,5);
+
+console.log(numbers);
+
+const fruits = ['apples', 'oranges', 'pears'];
+
+fruits[3] = 'grapes';
+
+fruits.push('mangos');
+
+fruits.unshift('strawberries');
+
+console.log(fruits);
+
+const animals = ['lions', 'tigers', 'bears'];
+
+console.log(animals);
+
+console.log(Array.isArray(animals));
+
+console.log(fruits.indexOf('oranges'));
+
+
+
 
 
 
