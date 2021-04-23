d824a998ba8e0d6bf9950d66294a75dfe53d9c31
Hemanth Panditi <panditih@mail.gvsu.edu>
2021-04-22 20:00:35 -0400

    Update hello.js

--- hello.js
+++ hello.js
@@ -2,6 +2,30 @@ console.log("Hello World. This is Hemanth Panditi");
 
 console.log("Welcome to a Javascript website");
 
+// var, let, const
+
+let age = 30;
+
+console.log(age);
+
+let score;
+
+score = 10;
+
+console.log(score);
+
+// String, Numbers, Boolean, null, undefined, Symbol
+
+const name = 'John';
+const age = 30;
+const isCool = true;
+const rating = 4.5;
+const x = null;
+const y = undefined;
+let z;
+
+
+
 
 
 
